ac3fb39a298ef108fb9170284149ef769c280bac
emuzemil900 <143541381+emuzemil900@users.noreply.github.com>
2023-11-27 12:48:28 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function initMap(){
     icon: 'images/icon.png'
   });
 
-  var contentString = '<h1>Sapparo</h1><p>Sapporo is the capital of Hokkaido, the second largest and northernmost of Japan's four major islands. It is notably a popular stop for winter sports such as skiing and snowboarding.</p>';
+  var contentString = '<h1>Sapparo</h1><p>Sapporo is the capital of Hokkaido, the second largest and northernmost of Japan's four major islands. It is notably a popular stop to watch and play winter sports like skiing and snowboarding.</p>';
 
   var infowindow = new google.maps.InfoWindow ({
     content: contentString
