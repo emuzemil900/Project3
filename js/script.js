ebedb70bd414b3723bd15cc776ea2efc509ab868
emuzemil900 <143541381+emuzemil900@users.noreply.github.com>
2023-11-29 13:21:43 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-function initMap(){
-  var el = document.getElementById('map-container');
-  var myLocation = new google.maps.LatLng(43.0609, 141.3549);
-  
-  var mapOptions = {
+function initMap() {
+  const myLocation = { lat: 43.0609, lng: 141.3549 };
+  const map = new google.maps.Map(document.getElementById("map-container"), {
+    zoom: 4,
     center: myLocation,
-    zoom: 20,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-    }
-  };
-
-  var map = new google.maps.Map(el, mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: myLocation,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    icon: 'images/icon.png'
   });
+
+  new google.maps.Marker({
+    position: myLocation,
+    map,
+    title: "Hello World!",
+  });
+}
 
   var contentString = '<h1>Sapparo</h1><p>Sapporo is the capital of Hokkaido, the second largest and northernmost of Japan's four major islands. It is notably a popular stop to watch and play winter sports like skiing and snowboarding.</p>';
 
@@ -28,7 +19,7 @@ function initMap(){
   });
 
   google.maps.event.addListener(marker, 'mouseover', function() {
-    infowindow.open(map, marker);
+    infowindow.open(map);
   });
 }
 
